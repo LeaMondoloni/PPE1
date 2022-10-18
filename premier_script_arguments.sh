80162faa3685888e5588c1163ba7212eb16a9877
LeaMondoloni <lea.mondoloni@gmail.com>
2022-10-18 22:36:06 +0200

    modifications script avec aruguments

--- premier_script_arguments.sh
+++ premier_script_arguments.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-ANNEE=$1
-TYPE=$2
+#On ajoute une nouvelle variable chemin pour éviter d'utiliser la commande cd
+CHEMIN=$1
+ANNEE=$2
+TYPE=$3
 
 echo "pour l'année $ANNEE" >> sortie2.txt
-grep "$TYPE" $ANNEE*.ann | wc -l >> sortie2.txt
+grep "$TYPE" $CHEMIN$ANNEE*.ann | wc -l >> sortie2.txt
