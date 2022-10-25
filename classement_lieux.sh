d9a10a0715d0b9ec7e45c6985d3cef54d6698887
LeaMondoloni <114879108+LeaMondoloni@users.noreply.github.com>
2022-10-25 19:00:31 +0200

    Update classement_lieux.sh

--- classement_lieux.sh
+++ classement_lieux.sh
@@ -13,7 +13,7 @@ grep "Location" $CHEMIN$ANNEE"_"$MOIS*.ann | rev | cut -d$'\t' -f1 | rev | sort 
 # Mon premier rev sert à retourner les lignes de mon fichier
 # cut sert sur mes lignes inversées à garder uniquement la première colonne de chaque ligne -d$ me permet d'indiquer le délimiteur de chaque (ici une tabulation) et le -f de donner la colonne qui m'intéresse
 # rev me permet de remettre à l'endroit les caractères
-# sort trie les lignes
-# uniq avec l'option -c me met les doublons sur une seule ligne avec leur nombre de répétition
+# sort trie les lignes par ordre alphabétique, ce qui permet de mettre les mêmes leiux les uns en dessous des autres
+# uniq avec l'option -c me met les doublons sur une seule ligne avec leur nombre de répétition (commande possible grâce au premier sort, car uniq n'agit que sur les lignes adjacentes)
 # sort trie à nouveau mes résultats -nr par ordre décroissant (-n étant pour trier des nombres par ordre croissant, -r les inverse)
 # head -n suivie de la variable nombre de lieux permettra d'afficher seulement les x lieux les plus cités.
