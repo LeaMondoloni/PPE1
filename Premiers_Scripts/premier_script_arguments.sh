106f2952547688139e7148829f93f7ca02966bbe
LeaMondoloni <114879108+LeaMondoloni@users.noreply.github.com>
2022-10-26 09:09:05 +0200

    Delete Premiers_Scripts directory

--- Premiers_Scripts/premier_script_arguments.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#On ajoute une nouvelle variable chemin pour éviter d'utiliser la commande cd
-CHEMIN=$1
-ANNEE=$2
-TYPE=$3
-
-echo "pour l'année $ANNEE" >> sortie2.txt
-grep "$TYPE" $CHEMIN$ANNEE*.ann | wc -l >> sortie2.txt
